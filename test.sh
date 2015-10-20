12582ba97f03e22da346b6bfd4acd6df75ee0306
David Mitchell <balance.it@gmail.com>
2015-10-20 13:54:17 +1300

    commit

--- test.sh
+++ test.sh
@@ -2,3 +2,14 @@
 echo
 echo "__________.............--------TESTING STARTED--------.............__________"
 echo
+
+
+echo "attempting to install etcd"
+
+curl -L  https://github.com/coreos/etcd/releases/download/v2.2.1/etcd-v2.2.1-linux-amd64.tar.gz -o etcd-v2.2.1-linux-amd64.tar.gz
+tar xzvf etcd-v2.2.1-linux-amd64.tar.gz
+cd etcd-v2.2.1-linux-amd64
+./etcd &
+
+./etcdctl set mykey "this is awesome"
+./etcdctl get mykey
