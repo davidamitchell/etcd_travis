7181ec51f553258ab09378f0229632ac1a1b0bf1
David Mitchell <balance.it@gmail.com>
2015-10-20 14:23:09 +1300

    commit

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
 console.log("TESTING STARTED")
+
+assert = require('assert');
+Etcd = require('node-etcd');
+
+etcd = new Etcd('127.0.0.1', '4001');
+etcd.set("mykey", "value");
+etcd.get("mykey", function(err, val){
+  console.log(val);
+
+
+  console.log(val.node.value);
+
+  assert.equal("value", val.node.value, 'expected to get "value" for key "mykey"', " something else");
+});
--- test.sh
+++ test.sh
@@ -9,7 +9,6 @@ echo "attempting to install etcd"
 curl -L  https://github.com/coreos/etcd/releases/download/v2.2.1/etcd-v2.2.1-linux-amd64.tar.gz -o etcd-v2.2.1-linux-amd64.tar.gz
 tar xzvf etcd-v2.2.1-linux-amd64.tar.gz
 cd etcd-v2.2.1-linux-amd64
-ls -la
 ./etcd &
 
 ./etcdctl --no-sync set mykey "this is awesome"
