48a51adab00943509a6babe764cc113d84951f41
David Mitchell <balance.it@gmail.com>
2015-10-20 16:31:27 +1300

    commit a

--- test.sh
+++ test.sh
@@ -6,9 +6,9 @@ FILE_ROOT="etcd-${VERSION}-linux-amd64"
 echo
 echo "Downloading ${VERSION} of etcd"
 
-curl -L  https://github.com/coreos/etcd/releases/download/${FILE_ROOT}.tar.gz -o ${FILE_ROOT}.tar.gz
-tar xzvf ${FILE_ROOT}.tar.gz
-cd ${FILE_ROOT}
+curl -L  https://github.com/coreos/etcd/releases/download/$FILE_ROOT.tar.gz -o $FILE_ROOT.tar.gz
+tar xzvf $FILE_ROOT.tar.gz
+cd $FILE_ROOT
 
 echo
 echo "Starting etcd"
