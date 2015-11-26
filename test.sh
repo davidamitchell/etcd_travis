3453a163f3d1357d6c853d0dc62d89677731a5ab
David Mitchell <balance.it@gmail.com>
2015-11-26 18:27:47 +1300

    something 5

--- test.sh
+++ test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 VERSION="v2.2.2"
-FILE_NAME="etcd-${VERSION}-linux-amd64.tar.gz"
+FILE_ROOT="etcd-${VERSION}-linux-amd64"
+FILE_NAME="${FILE_ROOT}.tar.gz"
 
-curl -L  "https://github.com/coreos/etcd/releases/download/${VERSION}/${FILE_NAME}" -o etcd.tar.gz
-tar xzvf etcd.tar.gz
-cd etcd
+curl -L  "https://github.com/coreos/etcd/releases/download/${VERSION}/${FILE_NAME}" -o "${FILE_NAME}"
+tar xzvf "${FILE_NAME}"
+cd $FILE_ROOT
 
 echo
 echo "Starting etcd"
